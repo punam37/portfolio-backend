b31015c6da745b8e99f808c77d07b534a3ea99a0
punam37 <poonamgautam802@gmail.com>
2026-03-16 17:48:39 +0530

    frontend url updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors({
 
-    origin:"http://localhost:5173",
+    origin:"https://portfolio-poonam.vercel.app",
     methods:["GET","POST"],
     credentials:true
 
